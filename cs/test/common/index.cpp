dd77b2c705d4a5a88f5490be901572fb6b7c9a5a
zzztttkkk <ztkisalreadytaken@gmail.com>
2022-03-17 22:37:41 +0800

    rust init

--- cs/test/common/index.cpp
+++
@@ -1,8 +0,0 @@
-#include <cs.h>
-#include <gtest/gtest.h>
-
-TEST(Common, rand) { FMT("{} {}\r\n", cs::rand(), cs::rand(0, 9999)); }
-
-TEST(Common, now) { FMT("{}\n", cs::now().time_since_epoch().count()); }
-
-TestMain;
