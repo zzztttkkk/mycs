bdaf670089776a1b064ea92f16290cf123dab44b
zzztttkkk <ztkisalreadytaken@gmail.com>
2022-03-18 21:09:55 +0800

    0.0

--- tests/algs_sorting.rs
+++ tests/algs_sorting.rs
@@ -2,7 +2,7 @@ use mycs::algs;
 
 #[test]
 fn test_bubble_sort() {
-    let mut vec = vec![1, 3, 4, 2];
-    algs::sorting::BUBBLE.sort(&mut vec);
-    println!("{:?}", vec);
+	let mut vec = vec![1, 3, 4, 2];
+	algs::sorting::BUBBLE.sort(&mut vec);
+	println!("{:?}", vec);
 }
